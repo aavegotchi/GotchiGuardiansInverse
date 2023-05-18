28d8ef3d1d3253fe1ba1088236773853527feedc
hzoells <hzoells@gmail.com>
2023-05-18 15:58:25 -0500

    chore: match file naming convention

--- Assets/UI/_Shared/Scripts/CountdownTimer_UI/CountdownTimer_UI_Model.cs
+++ Assets/UI/_Shared/Scripts/CountdownTimer_UI/CountdownTimer_UI_Model.cs
@@ -1,44 +1,40 @@
 using System;
 using PhaseManager.Presenter;
 
-namespace CountdownTimer_UI {
-    namespace Model 
+namespace CountdownTimer_UI.Model {
+    public class CountdownTimer_UI_Model
     {
-        public class CountdownTimer_UI_Model
+        #region Events
+        public event Action<bool> OnShowCountdownUIUpdated = delegate {};
+        public event Action<float> OnCountdownValueUpdated = delegate {};
+        #endregion
+
+        #region Public Variables
+        public bool ShowCountdownUI { get { return showCountdownUI; } }
+        public float CountdownValue { get { return countdownValue; } }
+        #endregion
+
+        #region Private Variables
+        private bool showCountdownUI = false;
+        private float countdownValue = 0f;
+        #endregion
+
+        public CountdownTimer_UI_Model()
         {
-            #region Events
-            public event Action<bool> OnShowCountdownUIUpdated = delegate {};
-            public event Action<float> OnCountdownValueUpdated = delegate {};
-            #endregion
+            PhasePresenter.Instance.OnShowCoundownUpdated += SetShowCountdownUI;
+            PhasePresenter.Instance.OnCountdownValueUpdated += SetCountdownValue;
+        }
 
-            #region Public Variables
-            public bool ShowCountdownUI { get { return showCountdownUI; } }
-            public float CountdownValue { get { return countdownValue; } }
-            #endregion
+        private void SetShowCountdownUI(bool isOpen)
+        {
+            showCountdownUI = isOpen;
+            OnShowCountdownUIUpdated(isOpen);
+        }
 
-            #region Private Variables
-            private bool showCountdownUI = false;
-            private float countdownValue = 0f;
-            #endregion
-
-            public CountdownTimer_UI_Model()
-            {
-                PhasePresenter.Instance.OnShowCoundownUpdated += SetShowCountdownUI;
-                PhasePresenter.Instance.OnCountdownValueUpdated += SetCountdownValue;
-            }
-
-            private void SetShowCountdownUI(bool isOpen)
-            {
-                showCountdownUI = isOpen;
-                OnShowCountdownUIUpdated(isOpen);
-            }
-
-            private void SetCountdownValue(float value)
-            {
-                countdownValue = value;
-                OnCountdownValueUpdated(countdownValue);
-            }
+        private void SetCountdownValue(float value)
+        {
+            countdownValue = value;
+            OnCountdownValueUpdated(countdownValue);
         }
     }
-    
 }
--- Assets/UI/_Shared/Scripts/CountdownTimer_UI/CountdownTimer_UI_Presenter.cs
+++ Assets/UI/_Shared/Scripts/CountdownTimer_UI/CountdownTimer_UI_Presenter.cs
@@ -6,123 +6,120 @@ using DG.Tweening;
 using TMPro;
 using CountdownTimer_UI.Model;
 
-namespace CountdownTimer_UI {
-    namespace Presenter
+namespace CountdownTimer_UI.Presenter {
+    public class CountdownTimer_UI_Presenter : MonoBehaviour
     {
-        public class CountdownTimer_UI_Presenter : MonoBehaviour
+        #region Fields
+        [Header("Sub Objects")]
+        [SerializeField]
+        private CanvasGroup canvasGroup;
+        [SerializeField]
+        private TextMeshProUGUI countdownLabel;
+        [SerializeField]
+        private RectTransform hourglassRT;
+
+        [Header("Hourglass Animation controls")]
+        [SerializeField]
+        private float loopDuration = 2.0f;
+        [SerializeField]
+        private AnimationCurve hourglassAnimCurve;
+
+        [Header("Debug Controls")]
+        [SerializeField]
+        private bool show = false;
+        #endregion
+
+        #region Private Variables
+        private bool isVisible = false;
+        private Tweener visibilityTween = null;
+
+        private CountdownTimer_UI_Model CountdownTimer_UI_Model = null;
+        #endregion
+
+        #region Unity Methods
+        void Start()
         {
-            #region Fields
-            [Header("Sub Objects")]
-            [SerializeField]
-            private CanvasGroup canvasGroup;
-            [SerializeField]
-            private TextMeshProUGUI countdownLabel;
-            [SerializeField]
-            private RectTransform hourglassRT;
-
-            [Header("Hourglass Animation controls")]
-            [SerializeField]
-            private float loopDuration = 2.0f;
-            [SerializeField]
-            private AnimationCurve hourglassAnimCurve;
-
-            [Header("Debug Controls")]
-            [SerializeField]
-            private bool show = false;
-            #endregion
-
-            #region Private Variables
-            private bool isVisible = false;
-            private Tweener visibilityTween = null;
-
-            private CountdownTimer_UI_Model CountdownTimer_UI_Model = null;
-            #endregion
-
-            #region Unity Methods
-            void Start()
-            {
-                if (show) {
-                    canvasGroup.alpha = 1;
-                } else {
-                    canvasGroup.alpha = 0;
-                }
-                CountdownTimer_UI_Model = new CountdownTimer_UI_Model();
-                CountdownTimer_UI_Model.OnShowCountdownUIUpdated += HandleUpdateShowCountdownUI;
-                CountdownTimer_UI_Model.OnCountdownValueUpdated += SetTimeLeft;
-
+            if (show) {
+                canvasGroup.alpha = 1;
+            } else {
+                canvasGroup.alpha = 0;
             }
+            CountdownTimer_UI_Model = new CountdownTimer_UI_Model();
+            CountdownTimer_UI_Model.OnShowCountdownUIUpdated += HandleUpdateShowCountdownUI;
+            CountdownTimer_UI_Model.OnCountdownValueUpdated += SetTimeLeft;
 
-            void Update()
+        }
+
+        void Update()
+        {
+            if (show != isVisible)
             {
-                if (show != isVisible)
+                if (show)
                 {
-                    if (show)
-                    {
-                        Show();
-                    }
-                    else
-                    {
-                        Hide();
-                    }
-                }
-            }
-            #endregion
-
-            #region public functions
-            private void Show() {
-                if (!isVisible) {
-                    show = true;
-                    isVisible = true;
-
-                    if (visibilityTween != null)
-                    {
-                        visibilityTween.Pause();
-                        visibilityTween.Kill();
-                        visibilityTween = null;
-                    }
-
-                    visibilityTween = canvasGroup.DOFade(1.0f, 0.5f);
-                    visibilityTween.OnComplete(() => visibilityTween = null);
-                }
-            }
-
-            private void Hide() {
-                if (isVisible) {
-                    show = false;
-                    isVisible = false;
-
-                    if (visibilityTween != null)
-                    {
-                        visibilityTween.Pause();
-                        visibilityTween.Kill();
-                        visibilityTween = null;
-                    }
-
-                    visibilityTween = canvasGroup.DOFade(0.0f, 0.5f);
-                    visibilityTween.OnComplete(() => visibilityTween = null);
-                }
-            }
-
-            private void SetTimeLeft(float timeLeft) {
-
-                float animTime = (timeLeft % loopDuration) / loopDuration;
-                float newVal = hourglassAnimCurve.Evaluate(animTime);
-                hourglassRT.localEulerAngles = new Vector3(0.0f, 0.0f, 360.0f * newVal);
-
-                countdownLabel.SetText(Mathf.Ceil(timeLeft).ToString());
-            }
-            #endregion
-
-            #region private functions
-            public void HandleUpdateShowCountdownUI(bool isOpen)
-            {
-                if (isOpen) {
                     Show();
-                } else {
+                }
+                else
+                {
                     Hide();
                 }
             }
-            #endregion
         }
+        #endregion
+
+        #region public functions
+        private void Show() {
+            if (!isVisible) {
+                show = true;
+                isVisible = true;
+
+                if (visibilityTween != null)
+                {
+                    visibilityTween.Pause();
+                    visibilityTween.Kill();
+                    visibilityTween = null;
+                }
+
+                visibilityTween = canvasGroup.DOFade(1.0f, 0.5f);
+                visibilityTween.OnComplete(() => visibilityTween = null);
+            }
+        }
+
+        private void Hide() {
+            if (isVisible) {
+                show = false;
+                isVisible = false;
+
+                if (visibilityTween != null)
+                {
+                    visibilityTween.Pause();
+                    visibilityTween.Kill();
+                    visibilityTween = null;
+                }
+
+                visibilityTween = canvasGroup.DOFade(0.0f, 0.5f);
+                visibilityTween.OnComplete(() => visibilityTween = null);
+            }
+        }
+
+        private void SetTimeLeft(float timeLeft) {
+
+            float animTime = (timeLeft % loopDuration) / loopDuration;
+            float newVal = hourglassAnimCurve.Evaluate(animTime);
+            hourglassRT.localEulerAngles = new Vector3(0.0f, 0.0f, 360.0f * newVal);
+
+            countdownLabel.SetText(Mathf.Ceil(timeLeft).ToString());
+        }
+        #endregion
+
+        #region private functions
+        public void HandleUpdateShowCountdownUI(bool isOpen)
+        {
+            if (isOpen) {
+                Show();
+            } else {
+                Hide();
+            }
+        }
+        #endregion
     }
 }
