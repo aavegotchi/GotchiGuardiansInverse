9ac2b8e81401d2a40be4d5c04d7476f558f42953
Tim Davis <timdavish@gmail.com>
2023-05-22 15:58:32 -0700

    feat: camera zoom and other adjustments

--- Assets/Cameras/Scripts/EnvironmentCameraSystem.cs
+++ Assets/Cameras/Scripts/EnvironmentCameraSystem.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 
 public class EnvironmentCameraSystem : MonoBehaviour
@@ -5,32 +6,56 @@ public class EnvironmentCameraSystem : MonoBehaviour
   #region Fields
   [Header("Required Refs")]
   [SerializeField] private GameObject bounds = null;
+  [SerializeField] private CinemachineVirtualCamera vCamera = null;
 
   [Header("Attributes")]
   [SerializeField] private bool isKeyboardMovementEnabled = true;
-  [SerializeField] private float keyboardMovementSpeed = 25f;
+  [SerializeField] private float keyboardMovementSpeed = 150f;
 
   [SerializeField] private bool isEdgeScrollMovementEnabled = true;
-  [SerializeField] private float edgeScrollBuffer = 50f;
-  [SerializeField] private float edgeScrollMovementSpeed = 100f;
-  
-  [SerializeField] private float boundsBuffer = 2.5f;
+  [SerializeField] private float edgeScrollMovementBuffer = 50f;
+  [SerializeField] private float edgeScrollMovementSpeedMax = 150f;
+  [SerializeField] private float edgeScrollMovementSpeedMin = 25f;
+
+  [SerializeField] private float movementBuffer = 0f;
+
+  [SerializeField] private bool isMouseZoomEnabled = true;
+  [SerializeField] private float mouseZoomAmount = 50f;
+  [SerializeField] private float mouseZoomSpeed = 5f;
+
+  [SerializeField] private float zoomDistanceMax = 300f;
+  [SerializeField] private float zoomDistanceMin = 125f;
+  [SerializeField] private float zoomAngleMax = 65f;
+  [SerializeField] private float zoomAngleMin = 80f;
   #endregion
 
   #region Private Variables
-  private Vector3 boundsMax = new Vector3(+Mathf.Infinity, +Mathf.Infinity, +Mathf.Infinity);
-  private Vector3 boundsMin = new Vector3(-Mathf.Infinity, -Mathf.Infinity, -Mathf.Infinity);
+  private bool isEnabled = true;
+
+  private Vector3 boundsMax = Vector3.positiveInfinity;
+  private Vector3 boundsMin = Vector3.negativeInfinity;
+
+  private CinemachineTransposer vCameraTransposer = null;
+
+  private Vector3 cameraPositionOffset = Vector3.zero;
   #endregion
 
   #region Unity Functions
   private void Awake()
   {
     SetBounds();
+
+    vCameraTransposer = vCamera.GetCinemachineComponent<CinemachineTransposer>();
+    cameraPositionOffset = vCameraTransposer.m_FollowOffset;
   }
 
   private void Update()
   {
-    HandleMovement();
+    if (isEnabled)
+    {
+      HandleMovement();
+      HandleZoom();
+    }
   }
   #endregion
 
@@ -59,7 +84,7 @@ public class EnvironmentCameraSystem : MonoBehaviour
       return;
     }
 
-    Vector3 boundsBufferVector = new Vector3(boundsBuffer, boundsBuffer, boundsBuffer);
+    Vector3 boundsBufferVector = new Vector3(movementBuffer, movementBuffer, movementBuffer);
 
     boundsMax = boundsRenderer.bounds.max + boundsBufferVector;
     boundsMin = boundsRenderer.bounds.min - boundsBufferVector;
@@ -71,52 +96,81 @@ public class EnvironmentCameraSystem : MonoBehaviour
 
     if (isKeyboardMovementEnabled)
     {
+      // Up
       if (Input.GetKey(KeyCode.W))
       {
         movementInputDirection.z += keyboardMovementSpeed;
       }
+
+      // Down
       if (Input.GetKey(KeyCode.S))
       {
         movementInputDirection.z -= keyboardMovementSpeed;
       }
+
+      // Left
       if (Input.GetKey(KeyCode.A))
       {
         movementInputDirection.x -= keyboardMovementSpeed;
       }
+
+      // Right
       if (Input.GetKey(KeyCode.D))
       {
         movementInputDirection.x += keyboardMovementSpeed;
       }
     }
 
-    if (isEdgeScrollMovementEnabled) {
+    if (isEdgeScrollMovementEnabled)
+    {
       Vector3 mouseViewportPoint = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
       bool isMouseOnScreen = !(
         mouseViewportPoint.x < 0 ||
         mouseViewportPoint.x > 1 ||
         mouseViewportPoint.y < 0 ||
         mouseViewportPoint.y > 1
       );
-      
-      if (isMouseOnScreen) {
-        if (Input.mousePosition.x <= edgeScrollBuffer) {
-          movementInputDirection.x -= edgeScrollMovementSpeed;
+
+      if (isMouseOnScreen)
+      {
+        // Up
+        float upBreakpoint = Screen.height - edgeScrollMovementBuffer;
+        if (Input.mousePosition.y >= upBreakpoint)
+        {
+          float movementSpeedStrength = (Input.mousePosition.y - upBreakpoint) / edgeScrollMovementBuffer;
+          movementInputDirection.z += GetEdgeScrollMovementSpeed(movementSpeedStrength);
         }
-        if (Input.mousePosition.y <= edgeScrollBuffer) {
-          movementInputDirection.z -= edgeScrollMovementSpeed;
+
+        // Down
+        float downBreakpoint = edgeScrollMovementBuffer;
+        if (Input.mousePosition.y <= downBreakpoint)
+        {
+          float movementSpeedStrength = (downBreakpoint - Input.mousePosition.y) / edgeScrollMovementBuffer;
+          movementInputDirection.z -= GetEdgeScrollMovementSpeed(movementSpeedStrength);
         }
-        if (Input.mousePosition.x >= Screen.width - edgeScrollBuffer) {
-          movementInputDirection.x += edgeScrollMovementSpeed;
+
+        // Left
+        float leftBreakpoint = edgeScrollMovementBuffer;
+        if (Input.mousePosition.x <= leftBreakpoint)
+        {
+          float movementSpeedStrength = (leftBreakpoint - Input.mousePosition.x) / edgeScrollMovementBuffer;
+          movementInputDirection.x -= GetEdgeScrollMovementSpeed(movementSpeedStrength);
         }
-        if (Input.mousePosition.y >= Screen.height - edgeScrollBuffer) {
-          movementInputDirection.z += edgeScrollMovementSpeed;
+
+        // Right
+        float rightBreakpoint = Screen.width - edgeScrollMovementBuffer;
+        if (Input.mousePosition.x >= rightBreakpoint)
+        {
+          float movementSpeedStrength = (Input.mousePosition.x - rightBreakpoint) / edgeScrollMovementBuffer;
+          movementInputDirection.x += GetEdgeScrollMovementSpeed(movementSpeedStrength);
         }
       }
     }
 
     Vector3 movementDirection = transform.forward * movementInputDirection.z + transform.right * movementInputDirection.x;
 
-    Vector3 newPosition = transform.position + (movementDirection * Time.deltaTime);
+    Vector3 newPosition = transform.position + (Time.deltaTime * movementDirection);
 
     newPosition = new Vector3(
       Mathf.Clamp(newPosition.x, boundsMin.x, boundsMax.x),
@@ -125,6 +179,54 @@ public class EnvironmentCameraSystem : MonoBehaviour
     );
 
     transform.position = newPosition;
+  }
+
+  private float GetEdgeScrollMovementSpeed(float movementSpeedStrength)
+  {
+    return (edgeScrollMovementSpeedMax - edgeScrollMovementSpeedMin) * movementSpeedStrength + edgeScrollMovementSpeedMin;
+  }
+
+  private void HandleZoom()
+  {
+    Vector3 cameraDirection = cameraPositionOffset.normalized;
+
+    if (isMouseZoomEnabled)
+    {
+      if (Input.mouseScrollDelta.y > 0)
+      {
+        // In
+        cameraPositionOffset -= cameraDirection * mouseZoomAmount;
+      }
+      if (Input.mouseScrollDelta.y < 0)
+      {
+        // Out
+        cameraPositionOffset += cameraDirection * mouseZoomAmount;
+      }
+    }
+
+    // Clamp zoom distance
+    if (cameraPositionOffset.magnitude > zoomDistanceMax)
+    {
+      cameraPositionOffset = cameraDirection * zoomDistanceMax;
+    }
+    if (cameraPositionOffset.magnitude < zoomDistanceMin)
+    {
+      cameraPositionOffset = cameraDirection * zoomDistanceMin;
+    }
+
+    // Change angle based on zoom level
+    float currentZoomPercent = (cameraPositionOffset.magnitude - zoomDistanceMin) / (zoomDistanceMax - zoomDistanceMin);
+    float newCameraAngle = (zoomAngleMax - zoomAngleMin) * currentZoomPercent + zoomAngleMin;
+
+    Vector3 newCameraPositionOffset = Quaternion.Euler(newCameraAngle, 0, 0) * -transform.forward * cameraPositionOffset.magnitude;
+
+    newCameraPositionOffset = Vector3.Lerp(
+      vCameraTransposer.m_FollowOffset,
+      newCameraPositionOffset,
+      Time.deltaTime * mouseZoomSpeed
+    );
+
+    vCameraTransposer.m_FollowOffset = newCameraPositionOffset;
   }
   #endregion
 }
